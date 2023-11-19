8216fa270302793ac7ea13d4f362454e42f3a50f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-11-19 21:44:29 +0200

    complete design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
-import daisyui from 'daisyui'
-
-
+import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        custom: {
+          primary: "#45AEF5",
+          secondary: "#222B3D",
+          accent: "#ff7a87",
+          neutral: "#2B3447",
+          "base-100": "#FFFFFF",
+          info: "#45AEF5",
+          success: "#009d3e",
+          warning: "#df3f00",
+          error: "#ff7a87",
+        },
+      },
+    ],
   },
   plugins: [daisyui],
-}
-
+};
